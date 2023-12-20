9240be91dd387d701d9149f67a0a836d40e84231
André Fajardo <117539643+andrefajardo@users.noreply.github.com>
2023-12-19 22:16:28 -0300

    Update nas listas de exercícios
    Conclusão da primeira sequência de exercícios e atualização da lista a ser praticada.
    
    <Resp: André Fajardo - Ref #000>

--- WarmUp/Java/semanaJava/src/conversorMoeda/ConversorMoeda.java
+++ WarmUp/Java/semanaJava/src/conversorMoeda/ConversorMoeda.java
@@ -1,9 +1,40 @@
 package conversorMoeda;
 
+import java.util.Scanner;
+
 public class ConversorMoeda {
-    public double vValor;
-    public String vMoeda;
-    public Double mainConversorMoeda(Double Valor, String Moeda) {
-        
+    public static void main(String[] args) {
+
+        Scanner conversor = new Scanner(System.in);
+        System.out.println("Informe o valor a converter: ");
+        Double vValor = conversor.nextDouble();
+        System.out.println("Informe a moeda para onde converter: ");
+        String vMoeda = conversor.next();
+
+        switch (vMoeda) {
+            case "UK":
+                System.out.println("O valor em libras é: " + (vValor * 1.2681));
+                break;
+            case "USD":
+                System.out.println("O valor em dolar americano é: " + (vValor * 0.2025));
+                break;
+            case "EUR":
+                System.out.println("O valor em euros é: " + (vValor * 0.1860));
+                break;
+            case "ARG":
+                System.out.println("O valor em peso argentino é: " + (vValor * 161));
+                break;
+            default:
+                System.out.println("Informação inválida!");
+        }
+
     }
 }
+
+//     EXERCÍCIOS 07
+//          João está planejando um mochilão. Seus destinos são: EUA, Inglaterra,
+//          França e planeja concluir na Argentina. Ele gostaria de um conversor de
+//          moedas para saber quanto o valor em Real vale em cada moeda. Implemente
+//          um conversor de moedas onde o usuário entrará com um valor em real,
+//          selecionará para qual moeda deseja fazer a conversão e o sistema irá
+//          retornar o valor na moeda selecionada.
---
+++ WarmUp/Java/semanaJava/src/estruturaEscolha/EstruturaEscolha.java
@@ -0,0 +1,5 @@
+package estruturaEscolha;
+
+public class EstruturaEscolha {
+    
+}
