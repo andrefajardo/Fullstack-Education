b412deaf5b2aa3fd6f3b29ad2c67c8c8d2c3a792
André Fajardo <117539643+andrefajardo@users.noreply.github.com>
2024-02-19 20:54:35 -0300

    Create - M1S5Ex8
    Criação do exercício n° 8, do primeiro módulo backend, semana5, no repositório.
    
    <Resp: André Fajardo - Ref #000>

---
+++ Módulo-1/M1S05Ex08/jogador8/src/Mensagens.java
@@ -0,0 +1,96 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Mensagens {
+
+    public static void msgErro() {
+        System.out.print("=======================================================\n");
+        System.out.print("=====                Opção INVÁLIDA!              =====\n");
+        System.out.print("=======================================================\n");
+
+    }
+
+    public static void mainMenu() {
+        System.out.println("\n=======================================================");
+        System.out.println("=====            Bem-vindo ao Game Play           =====");
+        System.out.println("=======================================================");
+        System.out.println("=====             Escolha uma opção:              =====");
+        System.out.println("=====   (c) para cadastrar jogador                =====");
+        System.out.println("=====   (l) para listar jogadores                 =====");
+        System.out.println("=====   (t) para listar todos jogadores           =====");
+        System.out.println("=====   (j) para jogar                            =====");
+        System.out.println("=====   (s) para sair                             =====");
+        System.out.println("=======================================================");
+        System.out.print("===== Opção: ");
+    }
+
+    public static void validaJogador() {
+        System.out.print("=======================================================\n");
+        System.out.print("==      Jogador já existe, informe outro nome.       ==\n");
+        System.out.print("=======================================================\n");
+    }
+
+    public static boolean JogadorInexistente() {
+        System.out.print("=======================================================\n");
+        System.out.print("==              Jogador não cadastrado!              ==\n");
+        System.out.print("=======================================================\n");
+        return true;
+    }
+
+    public static boolean DificuldadeJogo() {
+        System.out.print("=======================================================\n");
+        System.out.print("==          Informe o nível de dificuldade!          ==\n");
+        System.out.println("=====   (1) para fácil                           =====");
+        System.out.println("=====   (2) para intermediário                   =====");
+        System.out.println("=====   (3) para difícil                         =====");
+        System.out.print("=======================================================\n");
+        return true;
+    }
+
+    public static void mensaoHonrosa(ArrayList<Jogador> ranking)  {
+        for (int c = 0; c < ranking.size(); c++) {
+            if (ranking.get(c).isOnLine()) {
+                System.out.println("****************************************************************************");
+                System.out.println("              Mensão Honrosa: Jogador: " + ranking.get(c).getNome() + " - " + (c + 1) + "ª Posição");
+                System.out.println("****************************************************************************");
+            }
+        }
+    }
+
+    public static void melhorJogador() {
+        System.out.println("****************************************************************************");
+        System.out.println("              Melhor Jogador: " + Jogo.getMelhorJogador());
+        System.out.println("****************************************************************************");
+    }
+
+    public static void menuPedraPapelTesoura() {
+        System.out.println("\n=======================================================");
+        System.out.println("=====            Pedra | Papel | Tesoura          =====");
+        System.out.println("=======================================================");
+        System.out.println("=====             Escolha uma opção:              =====");
+        System.out.println("=====   (1) para Pedra                            =====");
+        System.out.println("=====   (2) para Papel                            =====");
+        System.out.println("=====   (3) para Tesoura                          =====");
+        System.out.println("=====   (0) para sair                             =====");
+        System.out.println("=======================================================");
+    }
+
+    public static void menuJogos() {
+        System.out.println("\n=======================================================");
+        System.out.println("=====               Escolha o Jogo                =====");
+        System.out.println("=======================================================");
+        System.out.println("=====             Escolha uma opção:              =====");
+        System.out.println("=====   (1) para Pedra Papel Tesoura              =====");
+        System.out.println("=====   (2) para Advinhe o número                 =====");
+        System.out.println("=======================================================");
+    }
+
+    public static void menuJogoAdivinhaNumero(int chances, int dificult) {
+        System.out.println("\n===============================================================");
+        System.out.println("=====        Adivinhe o número oculto de 0 a "+ dificult *10+"           =====");
+        System.out.println("=====        Você tem " + chances + " tentativa(s) para acertar         =====");
+        System.out.println("===============================================================");
+        System.out.print("Escolha um número: ");
+    }
+}
