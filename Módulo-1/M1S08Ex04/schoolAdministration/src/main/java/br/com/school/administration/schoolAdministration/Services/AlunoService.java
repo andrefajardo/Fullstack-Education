60eff5b4693e5f9bd530a5400af0b2d6fe2c8839
André Fajardo <117539643+andrefajardo@users.noreply.github.com>
2024-03-08 18:35:20 -0300

    Create - M1S08Ex04
    Elaboração do código conforme requisitos do Card-4 do M1S08Ex04, relativo ao módulo1, semana 08
    
    <Resp: André Fajardo - Ref #M1S08EX04>

---
+++ Módulo-1/M1S08Ex04/schoolAdministration/src/main/java/br/com/school/administration/schoolAdministration/Services/AlunoService.java
@@ -0,0 +1,32 @@
+package br.com.school.administration.schoolAdministration.Services;
+
+import br.com.school.administration.schoolAdministration.Models.AlunoModel;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class AlunoService {
+
+    public List<AlunoModel> buscarTodos() {
+        return AlunoModel.getAlunos();
+    }
+    public AlunoModel salvar (AlunoModel aluno) throws Exception {
+        if (validar(aluno)) {
+            return AlunoModel.inserir(aluno);
+        }
+        return null;
+    }
+    private boolean validar(AlunoModel aluno) throws Exception {
+        if (aluno.getNome().isBlank()) {
+            throw new Exception("Nome é obrigatório");
+        }
+
+        if (aluno.getDataNascimento() == null) {
+            throw new Exception("Data de nascimento é obrigatório");
+        }
+
+        return true;
+    }
+}
