4603c82bcd89a89a1bbb7873fa55e9daa2eedc69
André Fajardo <117539643+andrefajardo@users.noreply.github.com>
2024-07-01 15:43:50 -0300

    Elaboração do código conforme requisitos do Card-3 do M2S07Ex3, relativo ao módulo2, semana 7. <Resp: André Fajardo - Ref #M2S07Ex03>

--- Módulo-2/M2S71Ex01-07/portal-educacional/src/app/login/login.component.ts
+++ Módulo-2/M2S71Ex01-07/portal-educacional/src/app/login/login.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  constructor(private router: Router) {}
+
+  login = {
+    email:'',
+    senha:''
+  }
+
+  entrar(){
+    if(this.login.email && this.login.senha) {
+      this.router!.navigate(['/home']);
+    } else {
+      window.alert('Por favor, preencha os campos');
+    }
+  }
+  recuperar(){
+    window.alert("Processo de recuperação de senha enviado para o e-mail cadastrado")
+  }
 
 }
